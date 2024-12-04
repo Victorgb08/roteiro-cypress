595c7dacc13634e8014bbfafcd05f4eef538f733
Victor Batista <victor.guedes.08@gmail.com>
2024-12-04 17:54:49 -0300

    Adicionar 3 testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -46,7 +46,7 @@ describe('TODOMvc App', () => {
       .first()
       .click();
 
-    cy.get('[data-cy=filter-active-link')
+    cy.get('[data-cy=filter-active-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -54,7 +54,7 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'Prova de ES');
 
-    cy.get('[data-cy=filter-completed-link')
+    cy.get('[data-cy=filter-completed-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -62,10 +62,77 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'TP2 de ES');
 
-    cy.get('[data-cy=filter-all-link')
+    cy.get('[data-cy=filter-all-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 2);
+  });
+
+  it('Marca uma tarefa como completa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+  });
+
+  it('Desmarca uma tarefa como completa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed');
+  });
+  it('Marca e desmarca uma tarefa como completa', () => {
+    cy.visit('');
+  
+    // Adiciona uma nova tarefa
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+  
+    // Verifica se a tarefa foi adicionada
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+  
+    // Marca a tarefa como completa
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+  
+    // Verifica se a tarefa foi marcada como completa
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+  
+    // Desmarca a tarefa como completa
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+  
+    // Verifica se a tarefa foi desmarcada como completa
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed');
   });
 });
